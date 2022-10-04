41b5751bb808e34ab26cff9c98bc75f7ec69cf9e
dmitry <zhu@me.com>
2022-10-04 17:59:49 +0600

    {add OCR methods}

--- Enter_system/__main__.py
+++ Enter_system/__main__.py
@@ -1,8 +1,12 @@
 import torch
 import logging
-from .video_processing import apply_mask 
+from video_processing import apply_mask
 from datetime import datetime
-import pytz   
+import pytz
+import numpy as np
+import cv2
+import easyocr
+import re
 
 log = logging.getLogger('enter_system')
 
@@ -29,6 +33,37 @@ def plat_detection(img):
         if detected == 'plat-nomor':
             return cord_thres
     return None
+
+
+def process_image(path: str) -> np.array:
+    """
+    Function for preprocessing image for OCR recognition
+    :param path: path to image
+    :return: numpy array
+    """
+    image = cv2.imread(path)
+    image = cv2.resize(image, (0, 0), fx=3, fy=3)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (3, 3), 0)
+    thresh = cv2.threshold(blur, 50, 200, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+    invert = 255 - opening
+    return invert
+
+
+def ocr_recognition(path: str) -> str:
+    """
+    Function for OCR text from picture
+    :param path: image path
+    :return: recognized text
+    """
+    data = easyocr.Reader(['ru'], gpu=False)
+    text = data.readtext(process_image(path))
+    text = re.sub(r"[' :;~=+)()]", '', text[0][1]).lower()
+
+    return text
+
 
 def main_loop(video):
     # mask_video = 
