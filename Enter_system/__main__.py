f7bf2cb2a6c106ab75d7bb29a9c672baf49b22e2
zhuDmi <zhu@me.com>
2022-10-05 12:53:49 +0600

    change coord format in crop function

--- Enter_system/__main__.py
+++ Enter_system/__main__.py
@@ -2,34 +2,25 @@ import torch
 import logging
 import video_processing
 from datetime import datetime
-from pathlib import Path
-
+import pytz
+import numpy as np
 import cv2
 import easyocr
-import numpy as np
-import pytz
-import torch
-import yaml
+import re
+import gc
 from IPython.core.display import clear_output
 from PIL import Image
-
-import video_processing
-from yolov5.utils.dataloaders import LoadImages
-from yolov5.utils.general import check_file
 
 log = logging.getLogger('enter_system')
 
 yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-plat_yolo_model = torch.hub.load('ultralytics/yolov5', 'custom',
-                                 path=os.path.join(Path(__file__).parents[0],
-                                                   'models', 'plat_model'),
-                                 force_reload=True)
+plat_yolo_model = torch.hub.load('ultralytics/yolov5', 'custom', path='./models/plat_model.pt', force_reload=True)
 
 
-def car_detection(frame):
-    results = yolo_model(frame)
-    detections = results.pred[0][:, 5].unique()
-    labels = detections.cpu().numpy()
+# TODO check parameter for load in function?
+def car_detection(img):
+    results = yolo_model(img)
+    labels = results.xyxyn[0][:, -1].numpy(), results.xyxyn[0][:, :-1].numpy()
     names = yolo_model.names
     detected = '-'
     for label in labels:
@@ -109,35 +100,24 @@ def masking_video(path_video: str,
     clear_output()
 
 
-def worker():
-    with open(os.path.join(Path(__file__).parents[0], 'config', 'model.yaml')) as file:
-        config = yaml.load(file, Loader=yaml.Loader)
-    # masked_frames = masking_video(path_video=config['constants']['path_video'],
-    #                               frame_size=tuple(config['constants']['frame_size']),
-    #                               coord=config['constants']['coord'])
-    source = check_file('/second_4tb/kuchuganova/other/Number-detection/Enter_system/src/record.mp4')
-    masked_frames = LoadImages(source, img_size=(640, 640), stride=yolo_model.stride, auto=yolo_model.pt, vid_stride=1)
+def crop_image(image: np.array, coord: list) -> np.array:
+    """
+    Function for crop the image
+    :param image: image as np.array
+    :param coord: list
+    :return: np.array
+    """
+    img = Image.fromarray(image)
+    img_crop = img.crop(coord[0], coord[1], coord[2], coord[3])
 
-    for path, im, im0s, vid_cap, s in masked_frames:
-        detection = car_detection(im)
-        if detection == 'car' or detection == 'truck':
-            print('detect car')
-            moscow_time = datetime.now(pytz.timezone('Europe/Moscow'))
-            # TODO красивый вывод в лог
-            log.info(f'{detection.upper()} detected, start recording')
-            # TODO start saving video
-            # делать через out и просто write фрэймов
-            coord = plat_detection(im)
-            # TODO: coord crop
-            recognized_code = ocr_recognition()
-            log.info(f'Car - {recognized_code[:5]}')
-            with open(config['constant']['path_enter_car']) as file:
-                cars = file.readlines()
-            if recognized_code not in cars:
-                print('ALARM: CHECK CAR!!!!!!')
-            else:
-                log.info(f'Car - {recognized_code[:5]} - in passed')
+    return np.array(img_crop)
 
 
-if __name__ == '__main__':
-    worker()
+def main_loop(video):
+    # mask_video = 
+    # TODO: add mask on video 
+    detection = car_detection(video)
+    if detection == 'car' or detection == 'truck':
+        moscow_time = datetime.now(pytz.timezone('Europe/Moscow'))
+        log.info(f'{detection.upper()} detected, start recording')
+        # TODO start saving video
